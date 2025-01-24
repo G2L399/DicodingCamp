fb9de5d397d452d3f3eb1f280265c5821ce32f7d
Gamer2Language <bagassatwi@gmail.com>
2025-01-24 22:14:51 +0700

    done. no message

--- articlesData.js
+++ articlesData.js
@@ -16,7 +16,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Venus",
     imageSrc: "./img/venus.png",
     imageAlt: "Venus",
-    localHref: "./mercury/mercury.html",
+    localHref: "./venus/venus.html",
     content: `
         Venus is the second planet from the Sun. It is a terrestrial planet and is the closest in mass and size to its orbital neighbour Earth. Venus has by far the densest atmosphere of the terrestrial planets, composed mostly of carbon dioxide with a thick, global sulfuric acid cloud cover. At the surface it has a mean temperature of 737 K (464 °C; 867 °F) and a pressure 92 times that of Earth's at sea level. These extreme conditions compress carbon dioxide into a supercritical state at Venus's surface.
         <br /><br />
@@ -30,7 +30,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Earth",
     imageSrc: "./img/earth.jpg",
     imageAlt: "Venus",
-    localHref: "./mercury/mercury.html",
+    localHref: "./earth/earth.html",
     content: `
         Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust. The remaining 29.2% of Earth's crust is land, most of which is located in the form of continental landmasses within Earth's land hemisphere. Most of Earth's land is at least somewhat humid and covered by vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's groundwater, lakes, rivers, and atmospheric water combined. Earth's crust consists of slowly moving tectonic plates, which interact to produce mountain ranges, volcanoes, and earthquakes. Earth has a liquid outer core that generates a magnetosphere capable of deflecting most of the destructive solar winds and cosmic radiation.
         <br /><br />
@@ -42,7 +42,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/mars",
     imageSrc: "./img/mars.png",
     imageAlt: "mars",
-    localHref: "./mercury/mercury.html",
+    localHref: "./mars/mars.html",
     content: `
         Mars is the fourth planet from the Sun. The surface of Mars is orange-red because it is covered in iron(III) oxide dust, giving it the nickname "the Red Planet". Mars is among the brightest objects in Earth's sky, and its high-contrast albedo features have made it a common subject for telescope viewing. It is classified as a terrestrial planet and is the second smallest of the Solar System's planets with a diameter of 6,779 km (4,212 mi). In terms of orbital motion, a Martian solar day (sol) is equal to 24.6 hours, and a Martian solar year is equal to 1.88 Earth years (687 Earth days). Mars has two natural satellites that are small and irregular in shape: Phobos and Deimos.
         <br /><br />
@@ -54,7 +54,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Jupiter",
     imageSrc: "./img/jupiter.jpg",
     imageAlt: "jupiter",
-    localHref: "./mercury/mercury.html",
+    localHref: "./jupiter/jupiter.html",
     content: `
         Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than 2.5 times that of all the other planets in the Solar System combined and slightly less than one-thousandth the mass of the Sun. Its diameter is eleven times that of Earth, and a tenth that of the Sun. Jupiter orbits the Sun at a distance of 5.20 AU (778.5 Gm), with an orbital period of 11.86 years. It is the third-brightest natural object in the Earth's night sky, after the Moon and Venus, and has been observed since prehistoric times. Its name derives from that of Jupiter, the chief deity of ancient Roman religion.
         <br/><br/>
@@ -66,7 +66,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Saturn",
     imageSrc: "./img/Saturn.jpg",
     imageAlt: "Saturn",
-    localHref: "./mercury/mercury.html",
+    localHref: "./saturn/saturn.html",
     content: `
         Saturn is the sixth planet from the Sun and the second largest in the  Solar System, after Jupiter. It is a gas giant, with an average radius of about nine times that of Earth It has an eighth the average density of Earth, but is over 95 times more massive. Even though Saturn is almost as big as Jupiter, Saturn has less than a third its mass. Saturn orbits the Sun at a distance of 9.59 AU (1,434 million km), with an orbital period of 29.45 years.
         <br/><br/>
@@ -80,7 +80,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Uranus",
     imageSrc: "./img/Uranus.png",
     imageAlt: "Uranus",
-    localHref: "./mercury/mercury.html",
+    localHref: "./uranus/uranus.html",
     content: `
         Uranus is the seventh planet from the Sun. It is a gaseous cyan-coloured ice giant. Most of the planet is made of water, ammonia, and methane in a supercritical phase of matter, which astronomy calls "ice" or volatiles. The planet's atmosphere has a complex layered cloud structure and has the lowest minimum temperature (49 K (−224 °C; −371 °F)) of all the Solar System's planets. It has a marked axial tilt of 82.23° with a retrograde rotation period of 17 hours and 14 minutes. This means that in an 84-Earth-year orbital period around the Sun, its poles get around 42 years of continuous sunlight, followed by 42 years of continuous darkness.
         <br/><br/>
@@ -94,7 +94,7 @@ export const articlesData = [
     href: "https://en.wikipedia.org/wiki/Neptune",
     imageSrc: "./img/Neptune.jpg",
     imageAlt: "Neptune",
-    localHref: "./mercury/mercury.html",
+    localHref: "./neptune/neptune.html",
     content: `
       Neptune is the eighth and farthest known planet from the Sun. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth. Compared to its fellow ice giant Uranus, Neptune is slightly more massive, but denser and smaller. Being composed primarily of gases and liquids, it has no well-defined solid surface, and orbits the Sun once every 164.8 years at an orbital distance of 30.1 astronomical units (4.5 billion kilometres; 2.8 billion miles). It is named after the Roman god of the sea and has the astronomical symbol ♆, representing Neptune's trident.
       <br/><br/>
